e04d0b92fd224c6b1f6a8b883907cc935521fc0c
Larissa Sevilha <sevilha.larissa10@gmail.com>
2023-11-01 14:33:32 -0400

    navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "home#index"
+  
+  get 'home/index'
+  get 'contact/index'
+  get 'project/index'
+  get 'about/index'
 end
